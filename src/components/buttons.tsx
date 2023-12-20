ff4de4cc78a9f60a956bdf19d33ce62f7a5f389f
Marvelous Ighodaro <70376880+marvelwilson@users.noreply.github.com>
2023-12-20 22:32:58 +0100

    initial commit

---
+++ src/components/buttons.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/style.css'
+export const Button = ({ ...props }) => {
+    return (
+        <div className='btn'>
+            <div className='btn-success'>Create free account</div>
+        </div>
+    )
+}
